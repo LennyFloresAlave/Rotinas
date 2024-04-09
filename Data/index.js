f6c0916c86a395f1a25f4f708d5f2609bf013806
unknown <lennyfloresalave@gmail.com>
2024-04-09 14:14:28 -0300

    Nova rotina

---
+++ Data/index.js
@@ -0,0 +1,44 @@
+const DATA = [
+    {
+        id:'01',
+        porcentagem:"20%",
+        titulo:"Leitura",
+        imagem:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ4K8YrLCekTz2lj236GCUTnfBoLNTE3nYg0e0VakYflPgiotVTEsHXgEkRSy8vPwaiBtU&usqp=CAU",
+        descricao:"Livros de romance"
+    },
+    {
+        id:'02',
+        porcentagem:'40%',
+  titulo:'ir pra escola',
+  imagem:'https://portalmatogrosso.com.br/wp-content/uploads/2023/04/90bb58a4d58ddb00c4b7ff85f0738d0d.jpeg',
+  descricao:'Para estudar'
+    },
+    {
+        id:'03',
+        porcentagem:'10%',
+        titulo:'Estudos para o ENEM',
+        imagem:'https://www.estudaqui.com/wp-content/uploads/2020/05/como-estudar-enem-sozinho.png',
+        descricao:'Para um futuro melhor'
+    },
+    {
+        id:'04',
+        porcentagem:'5%',
+        titulo:'Jantar',
+        imagem:'https://previews.123rf.com/images/nutalinakul/nutalinakul2004/nutalinakul200400008/143842426-jantar-com-bife-e-batata-no-prato-pratos-de-desenhos-animados-isolados-ilustra%C3%A7%C3%A3o-de-cor-doodle-de.jpg',
+        descricao:'Comer comida'
+    },
+    {
+        id:'05',
+        porcentagem:'10%',
+        titulo:'Skincare',
+        imagem:'https://i.pinimg.com/736x/ea/ff/48/eaff48848f0dc941adc908a01838f716.jpg',
+        descricao:'Se cuidar faz bem ' 
+    },
+    {
+        id:'06',
+        porcentagem:'15%',
+        titulo:'Dormir',
+        imagem:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRV-PipcwlEy9oBiBmyO5kumj0TgubX8jILPA&s',
+        descricao:'Dormir faz bem ' 
+    }
+]
